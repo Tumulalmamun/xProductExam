fad60d843a66c600c316c9e6313cf751f32581bc
Abdullah Al Mamun <Tumulalmamun@users.noreply.github.com>
2019-10-20 01:40:47 +0600

    Add files via upload

---
+++ ResearchWebstack.CommandLine/Options/ProgramOptions.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ResearchWebstack.CommandLine.Options
+{
+   
+    public class ProgramOptions
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        [Option('p', "runPython", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Python { get; set; }
+
+
+        [Option('u', "getUnitTest", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool UnitTest { get; set; }
+
+    }
+}
--- ResearchWebstack.CommandLine/Program.cs
+++ ResearchWebstack.CommandLine/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.Serialization;
+using CommandLine;
+using System.Net;
+using ResearchWebstack.CommandLine.Options;
 
-namespace ResearchWebstack.CommandLine
+
+namespace QuickStart
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            string ServiceUrl = "http://localhost:54033/XProductService.svc/Unittestresults/";
+            WebRequest theRequest = WebRequest.Create(ServiceUrl);
+            WebResponse theResponse = theRequest.GetResponse();
+
+            DataContractSerializer collenctionData = new DataContractSerializer(typeof(ResearchWebstack.CommandLine.XProductService.UnitTest[]));
+            var arrUnitTest = collenctionData.ReadObject(theResponse.GetResponseStream());
+            var list = arrUnitTest as ResearchWebstack.CommandLine.XProductService.UnitTest[];
+            Parser.Default.ParseArguments<ProgramOptions>(args)
+                   .WithParsed<ProgramOptions>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       //else
+                       //{
+                       //    Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                       //}
+                       if (o.Python)
+                       {
+                           Process.Start("C:\\Program Files\\nodejs\\node.exe");
+                       }
+                       //else
+                       //{
+                       //    Console.WriteLine($"Current Arguments: -v {o.Python}");
+                       //}
+                       if(o.UnitTest)
+                       {
+                           Console.WriteLine("Bolck:");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.ToString()}");
+                       }
+                   });
         }
     }
 }
